85e263b116dcf3cf0bbb8150be3f96c85f0165b7
Szymon Wysocki <szymon.wysocki.stud@pw.edu.pl>
2021-05-20 14:04:15 +0200

    fix lint warnings in employee.cpp

--- employee.cpp
+++ employee.cpp
@@ -3,18 +3,16 @@
 #include <iostream>
 #include "employee.h"
 #include <set>
+#include <utility>
 
 //Employee
 
 //constructor
-Employee::Employee(const unsigned int n_id, const unsigned int n_wage){
-    id = n_id;
-    wage = n_wage;
-}
+Employee::Employee(const unsigned int n_id, const unsigned int n_wage) : id(n_id), wage(n_wage), hoursWorked(0) {}
 
 //get functions
-unsigned int Employee::getSalary(){
-    return hoursWorked*wage + bonus();
+unsigned int Employee::getSalary() {
+    return hoursWorked * wage + bonus();
 }
 
 unsigned int Employee::getId() const {
@@ -26,29 +24,23 @@ unsigned int Employee::getId() const {
 
 //constructor
 Tester::Tester(unsigned int n_id, unsigned int n_wage, std::set<Genre> n_genres)
-                : Employee(n_id, n_wage), genres(n_genres) {};
+        : Employee(n_id, n_wage), genres(std::move(n_genres)) {};
 
 unsigned int Tester::bonus() {
     return genres.size() * 20;
 }
 
 bool Tester::canTest(Genre genre) {
-    std::set<Genre>::iterator element = genres.begin();
-
-    while(element != genres.end()){
-        if ((*element) == genre){
-            return true;
-        }
-    }
-    return false;
+    auto it = genres.find(genre);
+    return it != genres.end();
 }
 
 //Manager
 
 Manager::Manager(unsigned int n_id, unsigned int n_wage)
-                : Employee(n_id, n_wage){};
+        : Employee(n_id, n_wage), requestsCompleted(0) {};
 
-unsigned int Manager::bonus(){
+unsigned int Manager::bonus() {
     return requestsCompleted * 50;
 }
 
