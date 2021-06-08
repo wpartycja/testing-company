972bcbfe79cef8a20f79f06ef52d5d9218f255e1
Patrycja Wysocka <p.wysocka@stud.elka.pw.edu.pl>
2021-06-08 17:43:23 +0200

    changed for command line arguments

--- main.cpp
+++ main.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
 #include "employee.h"
 #include "simulation.h"
-#include <chrono>
 #include <thread>
+#include <sstream>
 
-int main() {
+int main(int argc, char* argv[]) {
+    // checking if the number of given arguments is adequate
+    if (argc != 5) {
+        std::cerr << "Usage: " << argv[0] << " <duration>" << " <number_of_testers " <<
+                     "<number_of_publishers>" << "<number_of_games>" << std::endl;
+        return 1;
+    }
 
-    auto sim = Simulation(80, 20, 2, 10);
+    // checking the type of given arguments
+    for (int i = 1; i < 5; i++) {
+        std::istringstream ss(argv[i]);
+        int x;
+        if (!(ss >> x)) {
+            std::cerr << "Invalid argument: " << argv[i] << '\n';
+            return 1;
+        } else if (!ss.eof()) {
+            std::cerr << "Trailing characters in number: " << argv[i] << '\n';
+            return 1;
+        }
+    }
+
+    //starting simulation
+
+    std::cout << "Welcome in Testing Games Company Simulator!\n" << std::endl;
+    auto sim = Simulation(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]),
+                          atoi(argv[4]));
     sim.start();
 
     return 0;
